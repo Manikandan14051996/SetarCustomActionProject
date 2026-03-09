b6426f03fee4ba991db0623d769fac8dc4c0eb92
manikana <manikandan.1.a.ext@nokia.com>
2026-03-09 11:08:10 +0530

    bug fixes done...

--- setar-solution/src/main/java/com.nokia.nsw.uiv/action/QueryCPEDevice.java
+++ setar-solution/src/main/java/com.nokia.nsw.uiv/action/QueryCPEDevice.java
@@ -80,7 +80,7 @@ public class QueryCPEDevice implements HttpAction {
         response.setResourceMac((String) device.getProperties().get("macAddress"));
         response.setResourceMacMTA((String) device.getProperties().get("macAddressMta"));
         response.setResourceManufacturer((String) device.getProperties().get("manufacturer"));
-        response.setResourceStatus((String) device.getProperties().get("OperationalState"));
+        response.setResourceStatus((String) device.getProperties().get("AdministrativeState"));
         response.setResourceDescription(device.getProperties().getOrDefault("description", "").toString());
 
         // Map resourceSN from localName (remove prefix)
